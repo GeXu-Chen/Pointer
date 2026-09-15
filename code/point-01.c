6da0de34b51e9948ffb7a3d881303f56088bb669
WALYZY <2964046078@qq.com>
2026-09-15 20:59:50 +0800

    作用1

--- code/point-01.c
+++ code/point-01.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 
+void swap(int* x,int* y);
+
 int main()
 {
-int a = 10;
-int* p = &a;
-printf("%d\n",*p);
+//任务1：读取改变值
+// int a = 10;
+// int* p = &a;
+// printf("%d\n",*p);
+// return 0;
+// //11
+
+//作用1：在该函数外调换2个数的值
+int a=10;
+int b=20;
+printf("%d %d\n",a,b);
+swap(&a,&b);
+printf("%d %d",a,b);
 return 0;
-//11
+}
+
+//任务2
+void swap(int* x,int* y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
 }
 
