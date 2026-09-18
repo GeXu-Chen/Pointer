d36bf5ccf97da93a4fdd55167a165843808d9a5f
WALYZY <2964046078@qq.com>
2026-09-18 21:06:31 +0800

    structure

--- code/structure.c
+++ code/structure.c
@@ -9,7 +9,7 @@
 //     double height;
 // };
 
-// //起别名：大名可以不写了
+// //取代名就不用写大名了
 // typedef struct 
 // {
 //     char name[100];
@@ -22,16 +22,16 @@
 //     struct Friend gf1;
 //     gf1.age=20;
 //     gf1.height=162;
-//     strcpy(gf1.name, "真白"); 
+//     strcpy(gf1.name, "???"); 
 
-//     struct Friend gf2 = {"符华",1000,172};
+//     struct Friend gf2 = {"????",1000,172};
 
-//     GF gf3 ={"菲八啾比",27};
+//     GF gf3 ={"?????",27};
 
 //     struct Friend arr[2]={gf1,gf2};
 //     for (int i=0;i<2;i++)
 //     {
-//         printf("姓名：%s ,年龄:%d ,身高:%1f\n",arr[i].name,arr[i].age,arr[i].height);
+//         printf("??????%s ,????:%d ,????:%1f\n",arr[i].name,arr[i].age,arr[i].height);
 //     }
 //     return 0;
 // }
@@ -49,20 +49,20 @@ void WriteGF (GF* st);
 int main()
 {
     GF g1 ={"MiHoYo",10};
-    printf("名字：%s ,年龄：%d\n",g1.name,g1.age);
+    printf("姓名%s ,年龄%d\n",g1.name,g1.age);
 
     WriteGF(&g1);
 
-    printf("函数外的值：%s ,%d",g1.name,g1.age);
+    printf("函数后信息%s ,%d",g1.name,g1.age);
 
     return 0;
 }
 
 void WriteGF (GF* st)
 {
-    printf("输入姓名：\n");
+    printf("姓名\n");
     scanf("%s",(*st).name);
-    printf("输入年龄：\n");
+    printf("年龄\n");
     scanf("%d",&((*st).age));
-    printf("函数中的值%s ,%d\n",(*st).name,(*st).age);
+    printf("函数内信息%s ,%d\n",(*st).name,(*st).age);
 }
