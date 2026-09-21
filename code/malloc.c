667872d82f236e689ba209d060bcd2560f956d08
WALYZY <2964046078@qq.com>
2026-09-21 21:22:26 +0800

    内存：修改内存大小

--- code/malloc.c
+++ code/malloc.c
@@ -13,7 +13,19 @@ int main()
     for (int i=0;i<10;i++)
     {
         *(p+i) = (i+1)*10;   //对于*(p+i)也可以写成p[i]效果一样 为p移动i个距离
-        printf("%d\n",p[i]);
+        //printf("%d\n",p[i]);
     }
+
+    int* pp = realloc(p,20 * sizeof(int));       //remalloc建立了一个新空间且前面的内容与旧空间内内容一致
+    for (int n=10;n<20;n++)
+    {
+        pp[n]=(n+1)*10;
+    }
+
+    for (int z=0;z<20;z++)
+    {       
+        printf("%d\n",pp[z]);
+    }
+
     return 0;
 }
